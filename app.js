ecfa2d70479c0b0ff677df09b90cfd55718275eb
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-15 21:32:44 -0300

    Codigos de respuesta

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 
 const http = require('http'); 
 http.createServer( (req, res) =>{
-    res.write('Hola Mundo'); 
+    //status: el nro se asocia al codigo de la peticion
+    // res.writeHead(404); 
+    // res.write('404 | Page Not found'); 
+
+    //ejemplo: 201 es un status de archivo creado
+    res.writeHead(201); 
+    res.write('File Created')
+
+    //esto le indica a mi Back End que termine el proceso de peticion
     res.end(); 
 })
 .listen( 8080 ); 
