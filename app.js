eceabe1b197f6220d96d4e4b427f9007a94382a5
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-17 10:38:34 -0300

    Static content serve

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 8080; 
-app.get('/', (req,res) =>{
-res.send('Home Page')})
+
+// Serve static content
+app.use(express.static('public'))
 
 app.get('/hola-mundo',  (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World from get 01')
 })
-
+/**
+ * dirname contiene el path, entonces concateno el resto de la ruta
+ */
 app.get('*',  (req, res) => {
-    res.send('404 | Page Not Found')
+    res.sendFile(__dirname + '/public/404.html')
   })
 
 app.listen(port, ()=>{
